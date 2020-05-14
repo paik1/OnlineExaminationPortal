be60261671e6bd897b03732036520650a5e034fb
code-with-RJ <53438912+code-with-RJ@users.noreply.github.com>
2020-05-15 02:50:45 +0530

    MSI
    Landing Page

---
+++ landing.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style1.css">
+    
+</head>
+<body>
+    <div class="header">
+        <h1> <span>O</span>nline <span>E</span>xamination <span>P</span>ortal</h1>
+     </div> 
+    <a href="index.php"><button class="btn btn-default">Get Started <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></button></a> 
+    <div class="About">
+        <p class="heading" style="font-family: 'Maven Pro', sans-serif;"> <span style="font-family: 'Abril Fatface', cursive;">A</span>bout</p>
+        <p class="underline"></p>
+        <p style="font-family: 'Source Code Pro', monospace; margin-top: 20px;">Online Examination Portal is a project that have been created by the intern team at Fouses Education under the guidance of Mr.Prashann Shekhar. The idea behind creating this project is, as we all know due to this COVID-19 pandemic everything became still, economy of every country has crashed to its worst and over all whole world has stopped. Even though arranging exams online with covering some security aspects can be real ease to students and faculties. This is a contribution to the education system which can be consider as a convenient option for both students and teachers to keep up with the some sort of tentative schedule so that students do not need to skip their education. Here teacher can arrange tests on different topics in a MCQ form and students can attend the tests according to their convenience. Students will be provided with the results immediately and record will be stored in their logged in account.</p>
+    </div>
+    <div class="team-heading">
+        <div class="heading" style="font-family: 'Maven Pro', sans-serif;"> <span style="font-family: 'Abril Fatface', cursive;">T</span>eam</div>
+        <div class="underline"></div>
+     </div>
+    <div class="team">
+        <div>
+            <p class="name" style="margin-bottom: 10px;"> <span style="color: crimson;">R</span>ajdeep <span style="color: crimson;">J</span>inegar</p>
+            <p class="post">Front-end Developer</p>
+            <img src="Untitled-1.png" alt="" srcset="" width="250px" style=" margin: 0 auto; display: block;border-radius: 50%;">
+        </div>
+        <div>
+            <p class="name" style="margin-bottom: 10px;"><span style="color: crimson;">K</span>iran <span style="color: crimson;">P</span>ai</p>
+            <p class="post">Back-end Developer</p>
+            <img src="Kiran.jpeg" alt="" srcset="" width="250px" height="auto" style=" margin: 0 auto; display: block;border-radius: 50%;">
+        </div>
+        <div>
+            <p class="name" style="margin-bottom: 10px;"><span style="color: crimson;">A</span>khil <span style="color: crimson;">L</span>axman</p>
+            <p class="post">Project Manager</p>
+            <img src="akhil.jpeg" alt="" srcset="" width="250px" height="250px" style=" margin: 0 auto; display: block;border-radius: 50%;">
+        </div>
+        <div>
+            <p class="name" style="margin-bottom: 10px;"><span style="color: crimson;">L</span>ikhita <span style="color: crimson;">P</span>uli</p>
+            <p class="post">Front-end Developer</p>
+            <img src="Untitled-1.png" alt="" srcset="" width="250px" style=" margin: 0 auto; display: block;border-radius: 50%;">
+        </div>
+        <div>
+            <p class="name" style="margin-bottom: 10px;"><span style="color: crimson;">L</span>asya <span style="color: crimson;">P</span>rasanna<span style="color: crimson;">V</span>ajrapu </p>
+            <p class="post">Back-end Developer</p>
+            <img src="Untitled-1.png" alt="" srcset="" width="250px" style=" margin: 0 auto; display: block;border-radius: 50%;">
+        </div>
+        
+    </div>
+     
+     
+</body>
+</html>
