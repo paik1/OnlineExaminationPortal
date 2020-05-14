54d8d42c3026005f4967fad82c9154e7d50cdb07
Pai, Kiran <Kiran.Pai@emc.com>
2020-05-14 22:33:09 +0530

    Added timer function

--- qus_show.php
+++ qus_show.php
@@ -14,14 +14,49 @@ $_SESSION['cat'] = $cat;
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script>
+      function timeout()
+            {
+                var hours = Math.floor(timeLeft/3600);
+                var minute = Math.floor((timeLeft-(hours*60*60)-30)/60);
+                var second = timeLeft%60;
+                var hrs = checktime(hours);
+                var mint = checktime(minute);
+                var sec = checktime(second);
+                if(timeLeft<=0)
+                {
+                    clearTimeout(tm);
+                    document.getElementById("form1").submit();
+                }
+                else
+                {
+                    document.getElementById("time").innerHTML= hrs+":"+mint+":"+sec;
+                }
+                timeLeft--;
+                var tm = setTimeout(function(){timeout()},1000)
+            }
+            function checktime(msg)
+            {
+                if(msg<10)
+                {
+                    msg = "0"+msg;
+                }
+                return msg;
+            }
+  </script>
 </head>
-<body>
+<body onload="timeout()">
 
 <div class="container">
     <div class="col-sm-2"></div>
     <div class="col-sm-8">
-  <h2>Your exam has started</h2> 
-  <form action="ans.php" method="post"> 
+  <h2>Your exam has started 
+  <script type="text/javascript">
+    var timeLeft=2*60;
+  </script>
+  
+  <div id="time" style="float:right;">timeout</div></h2> 
+  <form id="form1" action="ans.php" method="post"> 
   <?php 
   $i = 1;
   foreach($qus->question as $qstn) {?>                
