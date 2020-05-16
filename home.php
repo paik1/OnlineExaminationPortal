00f2e23a1eb72cd6e7ab5ed266a35f8f723b4b61
Pai, Kiran <Kiran.Pai@emc.com>
2020-05-16 23:19:43 +0530

    Logout

--- home.php
+++ home.php
@@ -142,8 +142,13 @@ $profile->users_profile($email);
         </div>
     </div>
     <div id="menu3" class="tab-pane fade">
-      <h3>Menu 3</h3>
-      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        <center>
+      <h3>Would you like logout?</h3>
+
+      <button type="button" class="btn btn-danger"><a href="logout.php">Yes</a></button>
+      <button type="button" class="btn btn-success"><a href="home.php">No</a></button>
+      </center>
+      
     </div>
   </div>
 </div>
